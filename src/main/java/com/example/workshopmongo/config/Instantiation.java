0aaf13e92d6ced0badb11ae725867fa321427ead
lucasSsalgado <lucssslucsss@gmail.com>
2022-01-04 21:22:29 -0300

    referenciando os posts do usuario

--- src/main/java/com/example/workshopmongo/config/Instantiation.java
+++ src/main/java/com/example/workshopmongo/config/Instantiation.java
@@ -22,7 +22,6 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private PostRepository postRepository;	
-
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -40,6 +39,8 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018") , "Partiu Viagem", "Vou viajar para São Paulo, Abraços", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei Feliz hoje", new AuthorDTO(maria));
 		
-		postRepository.saveAll(Arrays.asList(post1, post2));		
+		postRepository.saveAll(Arrays.asList(post1, post2));	
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 }
