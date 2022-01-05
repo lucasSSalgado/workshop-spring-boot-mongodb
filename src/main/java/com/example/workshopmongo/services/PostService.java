c3ec93a3ca4bd01a35262653dd22715482f55f95
lucasSsalgado <lucssslucsss@gmail.com>
2022-01-05 17:38:09 -0300

    consulta simples com query methods

--- src/main/java/com/example/workshopmongo/services/PostService.java
+++ src/main/java/com/example/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.example.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,7 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Obj not found!"));
 	}
 	
-	
+	public List<Post> findbyTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
